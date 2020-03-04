b308a54616cae6a78949f985e462ebd18add05dd
fumoboy007 <fumoboy007@me.com>
2020-03-03 20:55:58 -0800

    Updated copyright year

--- DMPreferencesWindow/PreferencesWindowController.swift
+++ DMPreferencesWindow/PreferencesWindowController.swift
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright © 2018-2019 Darren Mo.
+// Copyright © 2018-2020 Darren Mo.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
